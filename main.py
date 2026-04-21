e4e50c0a4fa0e3df368a05a2e6d6ab88a6bb1113
topikal009-coder <topikal009@gmail.com>
2026-04-21 19:24:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ logger = logging.getLogger(__name__)
 # --- КОНФИГ ---
 API_ID = 30032542
 API_HASH = "ce646da1307fb452305d49f9bb8751ca"
-BOT_TOKEN = os.environ.get('BOT_TOKEN', '8711240311:AAHy5FzxQ7P0MpSm3Bv7xfoYDa9kVlwAb5w')
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '8659319275:AAFn4M8tJDkh_frsnLEFIutrQDKVB9gpm9k')
 
 # === КРИТИЧЕСКИ ВАЖНО: ПРАВИЛЬНАЯ РАБОЧАЯ ДИРЕКТОРИЯ ДЛЯ RAILWAY ===
 IS_RAILWAY = os.path.exists('/app') or 'RAILWAY_SERVICE_NAME' in os.environ
